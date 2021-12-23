beb6e7fb8fd284fc7709d3fea3cf3bd6e1b7771d
DatPhatNguyen <phatdat040801@gmail.com>
2021-12-23 20:38:59 +0700

    update

---
+++ js/detail.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // $('.show-child img').click(function(){
+    //     $('.show-child img')
+    // });
+    var img = document.createElement('img');
+    img.src = "/img/all/20190510_161058_148527_qua-trung-ga.max-800x800.jpg";
+    var block = document.getElementById("box-product");
+    block.appendChild(img);
+});
